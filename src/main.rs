db5c6df6496c2694d446e6c60500429a2b6b2ba0
Valerian G <valerian@dusklabs.io>
2022-02-14 18:36:17 +0000

    make code safer, use libmpv instead of the sys crate, fix segfault

--- src/main.rs
+++ src/main.rs
@@ -1,230 +1,9 @@
+pub mod video;
+
 slint::include_modules!();
 
-use glow::HasContext;
 use std::os::raw::c_void;
-use std::os::raw::c_char;
-use std::ffi::CStr;
-use std::ffi::CString;
-use std::ptr;
-use std::mem::transmute;
-
-use libmpv_sys::*;
-
-unsafe extern "C" fn get_proc_address(ctx: *mut c_void, proc: *const c_char) -> *mut c_void {
-    let proc_name = CStr::from_ptr(proc).to_str().unwrap();
-
-    (*(ctx as *const &dyn Fn(&str) -> *mut c_void))(proc_name) as *mut c_void
-}
-
-struct EGLUnderlay {
-    gl: glow::Context,
-    fbo: glow::Framebuffer,
-    texture: glow::Texture,
-    start_time: instant::Instant,
-    mpv_handle: *mut mpv_handle,
-    mpv_gl: *mut mpv_render_context,
-    program: glow::Program,
-    vao: glow::VertexArray,
-    vbo: glow::NativeBuffer,
-}
-
-impl EGLUnderlay {
-    fn new(gl: glow::Context, proc_addr_ctx: *mut c_void, file: &str) -> Self {
-        unsafe {
-            let mpv_handle = mpv_create();
-            assert!(mpv_initialize(mpv_handle) >= 0);
-            let log_level = CString::new("debug").unwrap();
-            mpv_request_log_messages(mpv_handle, log_level.as_ptr());
-
-            let mut params = [
-                mpv_render_param {
-                    type_: mpv_render_param_type_MPV_RENDER_PARAM_API_TYPE,
-                    data: MPV_RENDER_API_TYPE_OPENGL as *const _ as *mut c_void,
-                },
-                mpv_render_param {
-                    type_: mpv_render_param_type_MPV_RENDER_PARAM_OPENGL_INIT_PARAMS,
-                    data: &mut mpv_opengl_init_params {
-                        get_proc_address: Some(get_proc_address),
-                        get_proc_address_ctx: proc_addr_ctx,
-                        extra_exts: ptr::null(),
-                    } as *mut _ as *mut c_void,
-                },
-                mpv_render_param {
-                    type_: mpv_render_param_type_MPV_RENDER_PARAM_ADVANCED_CONTROL,
-                    data: &mut 0 as *mut i32 as *mut c_void,
-                },
-                mpv_render_param {
-                    type_: 0,
-                    data: ptr::null_mut(),
-                }
-            ];
-
-            let mut mpv_gl: *mut mpv_render_context = ptr::null_mut();
-            let result = mpv_render_context_create(&mut mpv_gl as *mut _, mpv_handle, params.as_mut_slice().as_mut_ptr());
-            assert!(result >= 0);
-
-            let command_type = CString::new("loadfile").unwrap();
-            let file = CString::new(file).unwrap();
-            let mut command = [
-                command_type.as_ptr(),
-                file.as_ptr(),
-                ptr::null(),
-            ];
-
-            let result = mpv_command(mpv_handle, command.as_mut().as_mut_ptr());
-
-            let program = gl.create_program().expect("Cannot create program");
-
-            let shader_sources = [
-                (glow::VERTEX_SHADER, include_str!("./vertex.glsl")),
-                (glow::FRAGMENT_SHADER, include_str!("./fragment.glsl")),
-            ];
-
-            let mut shaders = Vec::with_capacity(shader_sources.len());
-
-            for (shader_type, shader_source) in shader_sources.iter() {
-                let shader = gl.create_shader(*shader_type).expect("Cannot create shader");
-                gl.shader_source(shader, shader_source);
-                gl.compile_shader(shader);
-                if !gl.get_shader_compile_status(shader) {
-                    panic!("{}", gl.get_shader_info_log(shader));
-                }
-                gl.attach_shader(program, shader);
-                shaders.push(shader);
-            }
-
-            gl.link_program(program);
-            if !gl.get_program_link_status(program) {
-                panic!("{}", gl.get_program_info_log(program));
-            }
-
-            for shader in shaders {
-                gl.detach_shader(program, shader);
-                gl.delete_shader(shader);
-            }
-
-            let quad_verts: [f32; 24] = [
-                // positions   // texCoords
-                -1.0,  1.0,    0.0, 1.0,
-                -1.0, -1.0,    0.0, 0.0,
-                1.0,  -1.0,    1.0, 0.0,
-                -1.0,  1.0,    0.0, 1.0,
-                1.0,  -1.0,    1.0, 0.0,
-                1.0,   1.0,    1.0, 1.0
-            ];
-
-            let quad_vao = gl.create_vertex_array().unwrap();
-            let quad_vbo = gl.create_buffer().unwrap();
-
-            let vbo = gl.create_buffer().expect("Cannot create buffer");
-            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-
-            gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, quad_verts.align_to().1, glow::STATIC_DRAW);
-
-            let vao = gl.create_vertex_array().expect("Cannot create vertex array");
-            gl.bind_vertex_array(Some(vao));
-            gl.enable_vertex_attrib_array(0);
-            gl.vertex_attrib_pointer_f32(0, 2, glow::FLOAT, false, 16, 0);
-            gl.enable_vertex_attrib_array(1);
-            gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, 16, 8);
-
-            gl.bind_buffer(glow::ARRAY_BUFFER, None);
-            gl.bind_vertex_array(None);
-
-            /*
-            let screen_texture_location = gl.get_uniform_location(program, "screenTexture").unwrap();
-            gl.uniform_1_i32(Some(&screen_texture_location), 0);
-            */
-
-            let fbo = gl.create_framebuffer().unwrap();
-            gl.bind_framebuffer(glow::FRAMEBUFFER, Some(fbo));
-
-            let texture = gl.create_texture().unwrap();
-            gl.bind_texture(glow::TEXTURE_2D, Some(texture));
-            gl.tex_image_2d(glow::TEXTURE_2D, 0, glow::RGB as i32, 1920, 1080, 0, glow::RGB, glow::UNSIGNED_BYTE, None);
-            gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_MIN_FILTER, glow::LINEAR as i32);
-            gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_MAG_FILTER, glow::LINEAR as i32);
-
-            gl.framebuffer_texture_2d(glow::FRAMEBUFFER, glow::COLOR_ATTACHMENT0, glow::TEXTURE_2D, Some(texture), 0);
-
-            let depth_attachment = gl.create_texture().unwrap();
-            gl.bind_texture(glow::TEXTURE_2D, Some(depth_attachment));
-            gl.tex_storage_2d(glow::TEXTURE_2D, 1, glow::DEPTH24_STENCIL8, 1920, 1080);
-            gl.framebuffer_texture_2d(glow::FRAMEBUFFER, glow::DEPTH_STENCIL_ATTACHMENT, glow::TEXTURE_2D, Some(depth_attachment), 0);
-
-            assert_eq!(gl.check_framebuffer_status(glow::FRAMEBUFFER), glow::FRAMEBUFFER_COMPLETE);
-
-            gl.bind_framebuffer(glow::FRAMEBUFFER, None);
-
-            Self {
-                gl,
-                start_time: instant::Instant::now(),
-                mpv_handle,
-                mpv_gl,
-                fbo,
-                texture,
-                program,
-                vao,
-                vbo,
-            }
-        }
-    }
-}
-
-impl Drop for EGLUnderlay {
-    fn drop(&mut self) {
-        unsafe {
-        }
-    }
-}
-
-impl EGLUnderlay {
-    fn render(&mut self, rotation_enabled: bool) {
-        unsafe {
-            let gl = &self.gl;
-
-            gl.bind_framebuffer(glow::FRAMEBUFFER, Some(self.fbo));
-            gl.clear_color(0.1, 0.1, 0.1, 1.0);
-
-            let mut params = [
-                mpv_render_param {
-                    type_: mpv_render_param_type_MPV_RENDER_PARAM_OPENGL_FBO,
-                    data: &mut mpv_opengl_fbo {
-                        fbo: transmute(self.fbo),
-                        w: 1920,
-                        h: 1080,
-                        internal_format: 0,
-                    } as *mut _ as *mut c_void,
-                },
-
-                mpv_render_param {
-                    type_: mpv_render_param_type_MPV_RENDER_PARAM_FLIP_Y,
-                    data: &mut 1 as *mut i32 as *mut c_void,
-                },
-                mpv_render_param {
-                    type_: 0,
-                    data: ptr::null_mut()
-                },
-            ];
-
-            mpv_render_context_render(self.mpv_gl, params.as_mut().as_mut_ptr());
-            let elapsed = self.start_time.elapsed().as_millis() as f32;
-
-            gl.bind_framebuffer(glow::FRAMEBUFFER, None);
-            gl.clear_color(0.0, 0.0, 0.0, 1.0);
-            gl.clear(glow::COLOR_BUFFER_BIT);
-
-            gl.use_program(Some(self.program));
-            gl.bind_vertex_array(Some(self.vao));
-            gl.bind_texture(glow::TEXTURE_2D, Some(self.texture));
-            gl.draw_arrays(glow::TRIANGLES, 0, 6);
-
-            gl.bind_vertex_array(None);
-            gl.bind_texture(glow::TEXTURE_2D, None);
-            gl.use_program(None);
-        }
-    }
-}
+use video::VideoUnderlay;
 
 pub fn main() {
     let mut args = std::env::args();
@@ -234,10 +13,20 @@ pub fn main() {
     let app = App::new();
 
     let mut underlay = None;
+    let mut prev_is_paused = false;
 
     let app_weak = app.as_weak();
 
     if let Err(error) = app.window().set_rendering_notifier(move |state, graphics_api| {
+        let wh = if let Some(app) = app_weak.upgrade() {
+            let sf = app.window().scale_factor();
+            let phys_width = app.get_window_width() * sf;
+            let phys_height = app.get_window_height() * sf;
+            (phys_width, phys_height)
+        } else {
+            (2000.0, 2000.0)
+        };
+
         match state {
             slint::RenderingState::RenderingSetup => {
                 let (context, proc_addr) = match graphics_api {
@@ -247,11 +36,21 @@ pub fn main() {
                     _ => return,
                 };
 
-                underlay = Some(EGLUnderlay::new(context, proc_addr as *const _ as *mut c_void, &file))
+                underlay = Some(VideoUnderlay::new(context, proc_addr as *const _ as *mut c_void, &file, wh))
             }
             slint::RenderingState::BeforeRendering => {
                 if let (Some(underlay), Some(app)) = (underlay.as_mut(), app_weak.upgrade()) {
-                    underlay.render(app.get_rotation_enabled());
+                    let is_paused = app.get_is_paused();
+                    if prev_is_paused != is_paused {
+                        if is_paused {
+                            underlay.pause();
+                        } else {
+                            underlay.play();
+                        }
+
+                        prev_is_paused = is_paused;
+                    }
+                    underlay.render(wh);
                     app.window().request_redraw();
                 }
             }
---
+++ src/video.rs
@@ -0,0 +1,205 @@
+use glow::HasContext;
+
+use std::os::raw::c_void;
+use std::mem::transmute;
+
+use libmpv::*;
+use libmpv::render::*;
+
+fn get_proc_address(ctx: &*mut c_void, proc: &str) -> *mut c_void {
+    unsafe { (*((*ctx) as *const &dyn Fn(&str) -> *mut c_void))(proc) as *mut c_void }
+}
+
+pub struct VideoUnderlay {
+    gl: glow::Context,
+    fbo: glow::Framebuffer,
+    texture: glow::Texture,
+    depth_texture: glow::Texture,
+    start_time: instant::Instant,
+    program: glow::Program,
+    vao: glow::VertexArray,
+    vbo: glow::NativeBuffer,
+    prev_width: f32,
+    prev_height: f32,
+
+    // NOTE: The order of the fields below should not change otherwise we will get a SEGFAULT.
+    render_ctx: RenderContext,
+    mpv: Mpv,
+}
+
+impl VideoUnderlay {
+    pub fn new(gl: glow::Context, proc_addr_ctx: *mut c_void, file: &str, wh: (f32, f32)) -> Self {
+        let mut mpv = Mpv::new().expect("Error while creating MPV");
+        let render_ctx = RenderContext::new(
+            unsafe { mpv.ctx.as_mut() },
+            vec![
+            RenderParam::ApiType(RenderParamApiType::OpenGl),
+            RenderParam::InitParams(OpenGLInitParams {
+                get_proc_address,
+                ctx: proc_addr_ctx,
+            }),
+            ],
+        )
+            .expect("Failed creating render context");
+
+        mpv.event_context_mut().disable_deprecated_events().unwrap();
+        mpv.playlist_load_files(&[(file, FileState::AppendPlay, None)])
+            .unwrap();
+
+        Self::init_gl(gl, wh, mpv, render_ctx)
+    }
+
+    fn init_gl(gl: glow::Context, (width, height): (f32, f32), mpv: Mpv, render_ctx: RenderContext) -> Self {
+        unsafe {
+            let program = gl.create_program().expect("Cannot create program");
+
+            let shader_sources = [
+                (glow::VERTEX_SHADER, include_str!("./vertex.glsl")),
+                (glow::FRAGMENT_SHADER, include_str!("./fragment.glsl")),
+            ];
+
+            let mut shaders = Vec::with_capacity(shader_sources.len());
+
+            for (shader_type, shader_source) in shader_sources.iter() {
+                let shader = gl.create_shader(*shader_type).expect("Cannot create shader");
+                gl.shader_source(shader, shader_source);
+                gl.compile_shader(shader);
+                if !gl.get_shader_compile_status(shader) {
+                    panic!("{}", gl.get_shader_info_log(shader));
+                }
+                gl.attach_shader(program, shader);
+                shaders.push(shader);
+            }
+
+            gl.link_program(program);
+            if !gl.get_program_link_status(program) {
+                panic!("{}", gl.get_program_info_log(program));
+            }
+
+            for shader in shaders {
+                gl.detach_shader(program, shader);
+                gl.delete_shader(shader);
+            }
+
+            let quad_verts: [f32; 24] = [
+                // positions   // texCoords
+                -1.0,  1.0,    0.0, 1.0,
+                -1.0, -1.0,    0.0, 0.0,
+                1.0,  -1.0,    1.0, 0.0,
+                -1.0,  1.0,    0.0, 1.0,
+                1.0,  -1.0,    1.0, 0.0,
+                1.0,   1.0,    1.0, 1.0
+            ];
+
+            let vbo = gl.create_buffer().expect("Cannot create buffer");
+            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
+
+            gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, quad_verts.align_to().1, glow::STATIC_DRAW);
+
+            let vao = gl.create_vertex_array().expect("Cannot create vertex array");
+            gl.bind_vertex_array(Some(vao));
+            gl.enable_vertex_attrib_array(0);
+            gl.vertex_attrib_pointer_f32(0, 2, glow::FLOAT, false, 16, 0);
+            gl.enable_vertex_attrib_array(1);
+            gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, 16, 8);
+
+            gl.bind_buffer(glow::ARRAY_BUFFER, None);
+            gl.bind_vertex_array(None);
+
+            let fbo = gl.create_framebuffer().unwrap();
+            gl.bind_framebuffer(glow::FRAMEBUFFER, Some(fbo));
+
+            let texture = gl.create_texture().unwrap();
+            gl.bind_texture(glow::TEXTURE_2D, Some(texture));
+            gl.tex_image_2d(glow::TEXTURE_2D, 0, glow::RGB as i32, width as _, height as _, 0, glow::RGB, glow::UNSIGNED_BYTE, None);
+            gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_MIN_FILTER, glow::LINEAR as i32);
+            gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_MAG_FILTER, glow::LINEAR as i32);
+
+            gl.framebuffer_texture_2d(glow::FRAMEBUFFER, glow::COLOR_ATTACHMENT0, glow::TEXTURE_2D, Some(texture), 0);
+
+            let depth_texture = gl.create_texture().unwrap();
+            gl.bind_texture(glow::TEXTURE_2D, Some(depth_texture));
+            gl.tex_storage_2d(glow::TEXTURE_2D, 1, glow::DEPTH24_STENCIL8, 5000, 5000);
+            gl.framebuffer_texture_2d(glow::FRAMEBUFFER, glow::DEPTH_STENCIL_ATTACHMENT, glow::TEXTURE_2D, Some(depth_texture), 0);
+
+            assert_eq!(gl.check_framebuffer_status(glow::FRAMEBUFFER), glow::FRAMEBUFFER_COMPLETE);
+
+            gl.bind_framebuffer(glow::FRAMEBUFFER, None);
+            gl.bind_texture(glow::TEXTURE_2D, None);
+            gl.bind_vertex_array(None);
+            gl.bind_buffer(glow::ARRAY_BUFFER, None);
+
+            Self {
+                gl,
+                start_time: instant::Instant::now(),
+                fbo,
+                texture,
+                depth_texture,
+                program,
+                vao,
+                vbo,
+                prev_width: width,
+                prev_height: height,
+                mpv,
+                render_ctx
+            }
+        }
+    }
+
+    pub fn pause(&self) {
+        self.mpv.pause();
+    }
+
+    pub fn play(&self) {
+        self.mpv.unpause();
+    }
+
+    pub fn render(&mut self, (width, height): (f32, f32)) {
+        unsafe {
+            let gl = &self.gl;
+
+            gl.bind_framebuffer(glow::FRAMEBUFFER, Some(self.fbo));
+            gl.clear_color(0.1, 0.1, 0.1, 1.0);
+
+            if width != self.prev_width || height != self.prev_height {
+                gl.bind_texture(glow::TEXTURE_2D, Some(self.texture));
+                gl.tex_image_2d(glow::TEXTURE_2D, 0, glow::RGB as i32, width as _, height as _, 0, glow::RGB, glow::UNSIGNED_BYTE, None);
+                gl.bind_texture(glow::TEXTURE_2D, None);
+
+                self.prev_width = width;
+                self.prev_height = height;
+            }
+
+            self.render_ctx.render::<*mut c_void>(transmute(self.fbo), width as _, height as _, true)
+                .expect("Failed to render");
+
+            let elapsed = self.start_time.elapsed().as_millis() as f32;
+
+            gl.bind_framebuffer(glow::FRAMEBUFFER, None);
+            gl.clear_color(0.0, 0.0, 0.0, 1.0);
+            gl.clear(glow::COLOR_BUFFER_BIT);
+
+            gl.use_program(Some(self.program));
+            gl.bind_vertex_array(Some(self.vao));
+            gl.bind_texture(glow::TEXTURE_2D, Some(self.texture));
+            gl.draw_arrays(glow::TRIANGLES, 0, 6);
+
+            gl.bind_vertex_array(None);
+            gl.bind_texture(glow::TEXTURE_2D, None);
+            gl.use_program(None);
+        }
+    }
+}
+
+impl Drop for VideoUnderlay {
+    fn drop(&mut self) {
+        unsafe {
+            self.gl.delete_program(self.program);
+            self.gl.delete_buffer(self.vbo);
+            self.gl.delete_vertex_array(self.vao);
+            self.gl.delete_texture(self.texture);
+            self.gl.delete_texture(self.depth_texture);
+            self.gl.delete_framebuffer(self.fbo);
+        }
+    }
+}
